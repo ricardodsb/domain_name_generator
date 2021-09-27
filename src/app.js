cb4a706eb6ac72567b4e3aadec6feffeb3ca05e1
ricardodsb <ricardojdasilva15@gmail.com>
2021-09-27 17:40:53 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#generator").addEventListener("click", () => {
+    document.querySelector("#domain").innerHTML = domainGenerator();
+  });
 };
+
+let domainGenerator = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".net", ".es", ".uk"];
+  return (
+    randomElement(pronoun) +
+    randomElement(adj) +
+    randomElement(noun) +
+    randomElement(dom)
+  );
+};
+
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
